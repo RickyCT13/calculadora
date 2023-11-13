e78f95bab0b7ca439123e6328f5df31011a05226
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-11-13 10:35:39 +0100

    HLC Actividad 1.9 - Calculadora Básica

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,44 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
   val1: number | undefined;
   val2: number | undefined;
+  operation: string | undefined;
   result: number | string | undefined;
 
   constructor() {}
 
-  valuesAreSet() {
-    return (this.val1 && this.val2);
-  }
+  /**
+   * Suma dos valores
+   * @method
+   */
   add() {
-    if (this.val1 && this.val2) {
+    this.operation = 'Suma';
+    if (this.val1 && (this.val2 || this.val2 === 0)) {
       this.result = this.val1 + this.val2;
     }
   }
+
+  /**
+   * Resta dos valores
+   * @method
+   */
   subtract() {
-    if (this.val1 && this.val2) {
+    this.operation = 'Resta';
+    if (this.val1 && (this.val2 || this.val2 === 0)) {
       this.result = this.val1 - this.val2;
     }
   }
+
+  /**
+   * Calcula el producto de dos valores
+   * @method
+   */
   multiply() {
-    if (this.val1 && this.val2) {
+    this.operation = 'Producto';
+    if (this.val1 && (this.val2 || this.val2 === 0)) {
       this.result = this.val1 * this.val2;
     }
   }
+
+  /**
+   * Divide un valor por otro
+   * @method
+   */
   divide() {
+    this.operation = 'División';
     if (this.val1 && this.val2) {
-      if (this.val2 != 0) {
-        this.result = this.val1 / this.val2;
-      }
-      else {
-        this.result = "ERROR: No se puede dividir por 0";
-      }
-      
+      this.result = this.val1 / this.val2;
+    }
+    else {
+      this.result = 'ERROR: No se puede dividir por 0';
     }
   }
+
+  /**
+   * Eleva una base a un exponente
+   * @method
+   */
   power() {
-    if (this.val1 && this.val2) {
+    this.operation = 'Potencia';
+    if (this.val1 && (this.val2 || this.val2 === 0)) {
       this.result = Math.pow(this.val1, this.val2);
     }
+  }
+  
+  /**
+   * Reinicia los valores, el tipo de operación y el resultado.
+   * @method
+   */
+  reset() {
+    this.val1 = undefined;
+    this.val2 = undefined;
+    this.operation = undefined;
+    this.result = undefined;
   }
 }
